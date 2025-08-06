dc2c196176150d6afb07a2dcfd580f4e0533fa69
JustinFung <835776509@qq.com>
2025-08-06 10:13:09 +0800

    FP16 vector (scaler WIP)

--- layernorm_riscv.cpp
+++ layernorm_riscv.cpp
@@ -1,16 +1,6 @@
-// Xavier Hsinyuan is pleased to support the open source community by making ncnn available.
-//
-// Copyright (C) 2022 Xavier Hsinyuan <me@lstlx.com>. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+// Copyright 2024 Tencent
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include "layernorm_riscv.h"
 #include <math.h>
 
@@ -20,10 +10,6 @@
 #endif // __riscv_vector
 
 namespace ncnn {
-
-#if NCNN_ZFH
-#include "layernorm_rvv_fp16.h"
-#endif
 
 LayerNorm_riscv::LayerNorm_riscv()
 {
@@ -40,8 +26,10 @@ static inline int layernorm_rvv_pack1_procedure(int size, float* ptr, const floa
 {
     float sum = 0.f;
     float sqsum = 0.f;
-    vfloat32m1_t _sum = __riscv_vfmv_s_f_f32m1(0.f, __riscv_vsetvlmax_e32m1());
-    vfloat32m1_t _sqsum = __riscv_vfmv_s_f_f32m1(0.f, __riscv_vsetvlmax_e32m1());
+    size_t vl_max = __riscv_vsetvlmax_e32m1();
+    vfloat32m1_t _sum = __riscv_vfmv_s_f_f32m1(0.f, vl_max);
+    vfloat32m1_t _sqsum = __riscv_vfmv_s_f_f32m1(0.f, vl_max);
+
     {
         int n = size;
         float* ptr_sum = ptr;
@@ -50,11 +38,11 @@ static inline int layernorm_rvv_pack1_procedure(int size, float* ptr, const floa
             size_t vl = __riscv_vsetvl_e32m8(n);
             vfloat32m8_t _p = __riscv_vle32_v_f32m8(ptr_sum, vl);
             _sum = __riscv_vfredusum_vs_f32m8_f32m1(_p, _sum, vl);
-            // _sqsum = vfredusum_vs_f32m8_f32m1(_sqsum, vfmul_vv_f32m8(_p, _p, vl), /* scalar */ _sqsum, vl);
             ptr_sum += vl;
             n -= vl;
         }
     }
+
     sum = __riscv_vfmv_f_s_f32m1_f32(_sum);
     float mean = sum / size;
 
@@ -71,10 +59,10 @@ static inline int layernorm_rvv_pack1_procedure(int size, float* ptr, const floa
             ptr_sqsum += vl;
         }
     }
+
     sqsum = __riscv_vfmv_f_s_f32m1_f32(_sqsum);
     float var = sqsum / size;
-    // the var maybe minus due to accuracy
-    //float var = sqsum / size - mean * mean;
+
     float a = static_cast<float>(1.f / (sqrt(var + eps)));
     float b = -mean * a;
 
@@ -121,7 +109,6 @@ static inline int layernorm_rvv_pack1_procedure(int size, float* ptr, const floa
 
 static inline int layernorm_rvv_packn_procedure(int size, float* ptr, const float* gamma_data, const float* beta_data, float eps, int affine, const size_t vl)
 {
-    // mean and var
     vfloat32m1_t _sum = __riscv_vfmv_v_f_f32m1(0.f, vl);
     vfloat32m1_t _sqsum = __riscv_vfmv_v_f_f32m1(0.f, vl);
     for (int i = 0; i < size; i++)
@@ -138,11 +125,7 @@ static inline int layernorm_rvv_packn_procedure(int size, float* ptr, const floa
         _sqsum = __riscv_vfmacc_vv_f32m1(_sqsum, _p, _p, vl);
     }
     vfloat32m1_t _var = __riscv_vfdiv_vf_f32m1(_sqsum, size, vl);
-
-    // the var maybe minus due to accuracy
-    //float var = sqsum / size - mean * mean;
     vfloat32m1_t _a = __riscv_vfrdiv_vf_f32m1(__riscv_vfsqrt_v_f32m1(__riscv_vfadd_vf_f32m1(_var, eps, vl), vl), 1.f, vl);
-    // how about vfrsqrt7.v?
     vfloat32m1_t _b = __riscv_vfmul_vv_f32m1(__riscv_vfsgnjn_vv_f32m1(_mean, _mean, vl), _a, vl);
     if (affine)
     {
@@ -175,11 +158,8 @@ static inline int layernorm_scalar_procedure(int size, float* ptr, const float* 
     // mean and var
     float sum = 0.f;
     float sqsum = 0.f;
-    for (int i = 0; i < size; i++)
-    {
-        sum += ptr[i];
-        //sqsum += ptr[i] * ptr[i];
-    }
+    for (int i = 0; i < size; i++) sum += ptr[i];
+
     float mean = sum / size;
     float tmp = 0.f;
     for (int i = 0; i < size; i++)
@@ -187,27 +167,17 @@ static inline int layernorm_scalar_procedure(int size, float* ptr, const float* 
         tmp = ptr[i] - mean;
         sqsum += tmp * tmp;
     }
+
     float var = sqsum / size;
-    // the var maybe minus due to accuracy
-    //float var = sqsum / size - mean * mean;
 
     float a = static_cast<float>(1.f / (sqrt(var + eps)));
     float b = -mean * a;
 
     if (affine)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            ptr[i] = (ptr[i] * a + b) * gamma_data[i] + beta_data[i];
-        }
-    }
+        for (int i = 0; i < size; i++) ptr[i] = (ptr[i] * a + b) * gamma_data[i] + beta_data[i];
     else
-    {
-        for (int i = 0; i < size; i++)
-        {
-            ptr[i] = ptr[i] * a + b;
-        }
-    }
+        for (int i = 0; i < size; i++) ptr[i] = ptr[i] * a + b;
+
     return 0;
 }
 #endif // __riscv_vector
@@ -352,233 +322,4 @@ int LayerNorm_riscv::forward_inplace(Mat& bottom_top_blob, const Option& opt) co
 #endif // __riscv_vector
     return 0;
 }
-
-#if __riscv_vector && __riscv_zfh
-int LayerNorm_riscv::forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt) const
-{
-    // x = (x - mean) / sqrt(var + eps) * gamma + beta
-    int elempack = bottom_top_blob.elempack;
-    int dims = bottom_top_blob.dims;
-    int w = bottom_top_blob.w;
-
-    if (dims == 1)
-    {
-        __fp16* ptr = bottom_top_blob;
-
-        return layernorm_rvv_pack1_fp16s_procedure(w * elempack, ptr, gamma_data, beta_data, eps, affine);
-    }
-    if (elempack == 1)
-    {
-        if (dims == 2)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            // assert affine_size == w
-            #pragma omp parallel for num_threads(opt.num_threads)
-            for (int i = 0; i < h; i++)
-            {
-                __fp16* ptr = bottom_top_blob.row<__fp16>(i);
-                layernorm_rvv_pack1_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine);
-            }
-        }
-
-        if (dims == 3)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            int channels = bottom_top_blob.c;
-            int size = w * h;
-            if (affine_size == w)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    for (int i = 0; i < h; i++)
-                    {
-                        __fp16* ptr = bottom_top_blob.channel(q).row<__fp16>(i);
-
-                        layernorm_rvv_pack1_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine);
-                    }
-                }
-            }
-            else // if (affine_size == size)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    __fp16* ptr = bottom_top_blob.channel(q);
-                    layernorm_rvv_pack1_fp16s_procedure(size, ptr, gamma_data, beta_data, eps, affine);
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    const int packn = csrr_vlenb() / 2; // fp16
-    if (elempack == packn)
-    {
-        const size_t vl = vsetvl_e16m1(packn);
-        if (dims == 2)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            // assert affine_size == w
-
-            #pragma omp parallel for num_threads(opt.num_threads)
-            for (int i = 0; i < h; i++)
-            {
-                __fp16* ptr = bottom_top_blob.row<__fp16>(i);
-                layernorm_rvv_packn_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine, vl);
-            }
-        }
-        if (dims == 3)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            int channels = bottom_top_blob.c;
-            int size = w * h;
-
-            if (affine_size == w)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    for (int i = 0; i < h; i++)
-                    {
-                        __fp16* ptr = bottom_top_blob.channel(q).row<__fp16>(i);
-
-                        layernorm_rvv_packn_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine, vl);
-                    }
-                }
-            }
-            else // if (affine_size == size)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    __fp16* ptr = bottom_top_blob.channel(q);
-                    layernorm_rvv_packn_fp16s_procedure(size, ptr, gamma_data, beta_data, eps, affine, vl);
-                }
-            }
-        }
-    }
-
-    return 0;
-}
-
-int LayerNorm_riscv::forward_inplace_fp16sa(Mat& bottom_top_blob, const Option& opt) const
-{
-    // x = (x - mean) / sqrt(var + eps) * gamma + beta
-    int elempack = bottom_top_blob.elempack;
-    int dims = bottom_top_blob.dims;
-    int w = bottom_top_blob.w;
-
-    if (dims == 1)
-    {
-        __fp16* ptr = bottom_top_blob;
-
-        return layernorm_rvv_pack1_fp16sa_procedure(w * elempack, ptr, gamma_data, beta_data, eps, affine);
-    }
-    if (elempack == 1)
-    {
-        if (dims == 2)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            // assert affine_size == w
-            #pragma omp parallel for num_threads(opt.num_threads)
-            for (int i = 0; i < h; i++)
-            {
-                __fp16* ptr = bottom_top_blob.row<__fp16>(i);
-                layernorm_rvv_pack1_fp16sa_procedure(w, ptr, gamma_data, beta_data, eps, affine);
-            }
-        }
-
-        if (dims == 3)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            int channels = bottom_top_blob.c;
-            int size = w * h;
-            if (affine_size == w)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    for (int i = 0; i < h; i++)
-                    {
-                        __fp16* ptr = bottom_top_blob.channel(q).row<__fp16>(i);
-
-                        layernorm_rvv_pack1_fp16sa_procedure(w, ptr, gamma_data, beta_data, eps, affine);
-                    }
-                }
-            }
-            else // if (affine_size == size)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    __fp16* ptr = bottom_top_blob.channel(q);
-                    layernorm_rvv_pack1_fp16sa_procedure(size, ptr, gamma_data, beta_data, eps, affine);
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    const int packn = csrr_vlenb() / 2; // fp16
-    if (elempack == packn)
-    {
-        const size_t vl = vsetvl_e16m1(packn);
-        if (dims == 2)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            // assert affine_size == w
-
-            #pragma omp parallel for num_threads(opt.num_threads)
-            for (int i = 0; i < h; i++)
-            {
-                __fp16* ptr = bottom_top_blob.row<__fp16>(i);
-                layernorm_rvv_packn_fp16sa_procedure(w, ptr, gamma_data, beta_data, eps, affine, vl);
-            }
-        }
-        if (dims == 3)
-        {
-            int w = bottom_top_blob.w;
-            int h = bottom_top_blob.h;
-            int channels = bottom_top_blob.c;
-            int size = w * h;
-
-            if (affine_size == w)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    for (int i = 0; i < h; i++)
-                    {
-                        __fp16* ptr = bottom_top_blob.channel(q).row<__fp16>(i);
-
-                        layernorm_rvv_packn_fp16sa_procedure(w, ptr, gamma_data, beta_data, eps, affine, vl);
-                    }
-                }
-            }
-            else // if (affine_size == size)
-            {
-                #pragma omp parallel for num_threads(opt.num_threads)
-                for (int q = 0; q < channels; q++)
-                {
-                    __fp16* ptr = bottom_top_blob.channel(q);
-                    layernorm_rvv_packn_fp16sa_procedure(size, ptr, gamma_data, beta_data, eps, affine, vl);
-                }
-            }
-        }
-    }
-
-    return 0;
-}
-#endif
-
 } // namespace ncnn
--- layernorm_riscv_zfh.cpp
+++ layernorm_riscv_zfh.cpp
@@ -1,25 +1,16 @@
-// Xavier Hsinyuan is pleased to support the open source community by making ncnn available.
-//
-// Copyright (C) 2022 Xavier Hsinyuan <me@lstlx.com>. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+// Copyright 2024 Tencent
+// SPDX-License-Identifier: BSD-3-Clause
 
-// fp16s
-#ifndef __fp16
-#define __fp16 _Float16
-#endif
+#include "layernorm_riscv.h"
 
+#if __riscv_vector
 #include <riscv_vector.h>
 #include "riscv_usability.h"
+#endif // __riscv_vector
 
+namespace ncnn {
+
+#if NCNN_ZFH && __riscv_zvfh
 static inline int layernorm_rvv_pack1_fp16s_procedure(int size, __fp16* ptr, const float* gamma_data, const float* beta_data, float eps, int affine)
 {
     float sum = 0.f;
@@ -50,7 +41,7 @@ static inline int layernorm_rvv_pack1_fp16s_procedure(int size, __fp16* ptr, con
             size_t vl = __riscv_vsetvl_e16m4(n);
             vfloat32m8_t _p = __riscv_vfwcvt_f_f_v_f32m8(__riscv_vle16_v_f16m4(ptr_sqsum, vl), vl);
             _p = __riscv_vfsub_vf_f32m8(_p, mean, vl);
-            _sqsum = __riscv_vfredusum_vs_f32m8_f32m1(__riscv_vfmul_vv_f32m8(_p, _p, vl), _sqsum, vl);
+            _sqsum = __riscv_vfredusum_vs_f32m8_f32m1(__riscv_vfmul_vv_f32m8(_p, _p, vl), /* scalar */ _sqsum, vl);
             n -= vl;
             ptr_sqsum += vl;
         }
@@ -169,7 +160,7 @@ static inline int layernorm_rvv_pack1_fp16sa_procedure(int size, __fp16* ptr, co
         {
             size_t vl = __riscv_vsetvl_e16m8(n);
             vfloat16m8_t _p = __riscv_vle16_v_f16m8(ptr_sum, vl);
-            _sum = __riscv_vfredusum_vs_f16m8_f16m1(_p, _sum, vl);
+            _sum = __riscv_vfredusum_vs_f16m8_f16m1(_p, /* scalar */ _sum, vl);
             // _sqsum = vfredusum_vs_f32m8_f32m1(_sqsum, vfmul_vv_f32m8(_p, _p, vl), /* scalar */ _sqsum, vl);
             ptr_sum += vl;
             n -= vl;
@@ -186,7 +177,7 @@ static inline int layernorm_rvv_pack1_fp16sa_procedure(int size, __fp16* ptr, co
             size_t vl = __riscv_vsetvl_e16m8(n);
             vfloat16m8_t _p = __riscv_vle16_v_f16m8(ptr_sqsum, vl);
             _p = __riscv_vfsub_vf_f16m8(_p, mean, vl);
-            _sqsum = __riscv_vfredusum_vs_f16m8_f16m1(__riscv_vfmul_vv_f16m8(_p, _p, vl), _sqsum, vl);
+            _sqsum = __riscv_vfredusum_vs_f16m8_f16m1(__riscv_vfmul_vv_f16m8(_p, _p, vl), /* scalar */ _sqsum, vl);
             n -= vl;
             ptr_sqsum += vl;
         }
@@ -289,3 +280,118 @@ static inline int layernorm_rvv_packn_fp16sa_procedure(int size, __fp16* ptr, co
 
     return 0;
 }
+
+int LayerNorm_riscv::forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt) const
+{
+    // x = (x - mean) / sqrt(var + eps) * gamma + beta
+    int elempack = bottom_top_blob.elempack;
+    int dims = bottom_top_blob.dims;
+    int w = bottom_top_blob.w;
+
+    if (dims == 1)
+    {
+        __fp16* ptr = bottom_top_blob;
+
+        return layernorm_rvv_pack1_fp16s_procedure(w * elempack, ptr, gamma_data, beta_data, eps, affine);
+    }
+    if (elempack == 1)
+    {
+        if (dims == 2)
+        {
+            int w = bottom_top_blob.w;
+            int h = bottom_top_blob.h;
+            // assert affine_size == w
+            #pragma omp parallel for num_threads(opt.num_threads)
+            for (int i = 0; i < h; i++)
+            {
+                __fp16* ptr = bottom_top_blob.row<__fp16>(i);
+                layernorm_rvv_pack1_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine);
+            }
+        }
+
+        if (dims == 3)
+        {
+            int w = bottom_top_blob.w;
+            int h = bottom_top_blob.h;
+            int channels = bottom_top_blob.c;
+            int size = w * h;
+            if (affine_size == w)
+            {
+                #pragma omp parallel for num_threads(opt.num_threads)
+                for (int q = 0; q < channels; q++)
+                {
+                    for (int i = 0; i < h; i++)
+                    {
+                        __fp16* ptr = bottom_top_blob.channel(q).row<__fp16>(i);
+
+                        layernorm_rvv_pack1_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine);
+                    }
+                }
+            }
+            else // if (affine_size == size)
+            {
+                #pragma omp parallel for num_threads(opt.num_threads)
+                for (int q = 0; q < channels; q++)
+                {
+                    __fp16* ptr = bottom_top_blob.channel(q);
+                    layernorm_rvv_pack1_fp16s_procedure(size, ptr, gamma_data, beta_data, eps, affine);
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    const int packn = csrr_vlenb() / 2; // fp16
+    if (elempack == packn)
+    {
+        const size_t vl = __riscv_vsetvl_e16m1(packn);
+        if (dims == 2)
+        {
+            int w = bottom_top_blob.w;
+            int h = bottom_top_blob.h;
+            // assert affine_size == w
+
+            #pragma omp parallel for num_threads(opt.num_threads)
+            for (int i = 0; i < h; i++)
+            {
+                __fp16* ptr = bottom_top_blob.row<__fp16>(i);
+                layernorm_rvv_packn_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine, vl);
+            }
+        }
+        if (dims == 3)
+        {
+            int w = bottom_top_blob.w;
+            int h = bottom_top_blob.h;
+            int channels = bottom_top_blob.c;
+            int size = w * h;
+
+            if (affine_size == w)
+            {
+                #pragma omp parallel for num_threads(opt.num_threads)
+                for (int q = 0; q < channels; q++)
+                {
+                    for (int i = 0; i < h; i++)
+                    {
+                        __fp16* ptr = bottom_top_blob.channel(q).row<__fp16>(i);
+
+                        layernorm_rvv_packn_fp16s_procedure(w, ptr, gamma_data, beta_data, eps, affine, vl);
+                    }
+                }
+            }
+            else // if (affine_size == size)
+            {
+                #pragma omp parallel for num_threads(opt.num_threads)
+                for (int q = 0; q < channels; q++)
+                {
+                    __fp16* ptr = bottom_top_blob.channel(q);
+                    layernorm_rvv_packn_fp16s_procedure(size, ptr, gamma_data, beta_data, eps, affine, vl);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+#endif // NCNN_ZFH && __riscv_zvfh
+} // namespace ncnn
