ca3589f375fb558ca3c1ea925261416220d7d40d
JustinFung <835776509@qq.com>
2025-08-20 23:34:04 +0800

    Add failed XuanTie compiling script

--- build.sh
+++ build.sh
@@ -15,3 +15,18 @@ pushd build
 cmake -DNCNN_XTHEADVECTOR=OFF -DNCNN_BUILD_TESTS=ON -DNCNN_BUILD_TOOLS=ON -DNCNN_BUILD_EXAMPLES=ON -DCMAKE_TOOLCHAIN_FILE=/home/qfeng10/project/ncnn/toolchains/riscv64-unknown-linux-gnu.toolchain.cmake -DCMAKE_BUILD_TYPE=release ..
 make -j 12
 popd
+
+
+export RISCV_ROOT_PATH=~/toolchains/Xuantie-900-gcc-linux-6.6.0-glibc-x86_64-V3.2.0/
+mkdir -p build_thead
+pushd build_thead
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/c910-v310.toolchain.cmake -DCMAKE_BUILD_TYPE=release \
+      -DNCNN_OPENMP=ON -DNCNN_THREADS=ON \
+      -DNCNN_RUNTIME_CPU=OFF \
+      -DNCNN_RVV=OFF \
+      -DNCNN_XTHEADVECTOR=ON \
+      -DNCNN_ZFH=ON \
+      -DNCNN_ZVFH=OFF \
+      -DNCNN_SIMPLEOCV=ON -DNCNN_BUILD_EXAMPLES=ON -DNCNN_BUILD_TESTS=ON ..
+make -j 12
+popd
