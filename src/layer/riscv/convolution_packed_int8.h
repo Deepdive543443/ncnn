6d0953e154f8b7fef9fdc388738c5d8e3a178bf4
Deepdive543443 <83911295+Deepdive543443@users.noreply.github.com>
2026-02-17 10:14:26 +0000

    apply code-format changes

--- src/layer/riscv/convolution_packed_int8.h
+++ src/layer/riscv/convolution_packed_int8.h
@@ -1,4 +1,4 @@
 static void convolution_packed_int8(const Mat& bottom_blob, Mat& top_blob, const Mat& weight_data_tm, int kernel_w, int kernel_h, int dilation_w, int dilation_h, int stride_w, int stride_h, const Option& opt)
 {
-    // TODO: implement general packed int8 
+    // TODO: implement general packed int8
 }
