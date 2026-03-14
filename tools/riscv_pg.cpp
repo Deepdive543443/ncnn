10f2a1770cc1ebc62d23a74328ea55ec9e355b82
JustinFung <835776509@qq.com>
2026-03-14 18:56:09 +0800

    Packed Conv Experiment

--- tools/riscv_pg.cpp
+++ tools/riscv_pg.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <stdlib.h>
+
 #if __riscv_vector
 #include <riscv_vector.h>
 
@@ -33,17 +35,23 @@ int main(int argc, char** argv)
     vint8m2_t vs = __riscv_vmv_v_x_i8m2(2, vlmax_e32m8);
     vint8m2_t va = __riscv_vmv_v_x_i8m2(4, vlmax_e32m8);
     vint16m4_t vb = __riscv_vmv_v_x_i16m4(1, vlmax_e32m8);
-    
+
     vint32m8_t vresult = __riscv_vwadd_vv_i32m8(vb, __riscv_vwmul_vv_i16m4(vs, va, vlmax_e32m8), vlmax_e32m8);
 
     __riscv_vse32_v_i32m8(&s[0], vresult, vlmax_e32m8);
 
     std::cout << "[ ";
-    for (int32_t &rs : s) {
+    for (int32_t& rs : s)
         std::cout << rs << " ";
-    }
+
     std::cout << "]" << std::endl;
-    
+
+    std::vector<size_t> packs(static_cast<int>(std::log2(vlmax_e32m8 / 2)));
+    for (int i = 0; i < packs.size(); i++)
+        packs[i] = 1 << i;
+
+    for (size_t& i : packs)
+        std::cout << i << std::endl;
 
     return 0;
 #else
