e2c462ab2da999ecc3718df3de405cba6fe7c410
JustinFung <835776509@qq.com>
2026-06-06 08:51:26 +0800

    try expose

--- tests/test_shufflechannel.cpp
+++ tests/test_shufflechannel.cpp
@@ -3,6 +3,8 @@
 
 #include "testutil.h"
 
+#include "layer_type.h"
+
 static int test_shufflechannel(const ncnn::Mat& a, int group, int reverse)
 {
     ncnn::ParamDict pd;
@@ -29,6 +31,70 @@ static int test_shufflechannel(int w, int h, int d, int c, int group, int revers
 {
     return test_shufflechannel(RandomMat(w, h, d, c), group, reverse);
 }
+
+#if NCNN_RVV || NCNN_XTHEADVECTOR
+static int test_shufflechannel_pack4_non_optimal()
+{
+    const int packn = ncnn::cpu_riscv_vlenb() / 4;
+    if (packn <= 4)
+        return 0;
+
+    ncnn::Mat a = RandomMat(2, 3, 16);
+
+    ncnn::ParamDict pd;
+    pd.set(0, 2);
+    pd.set(1, 0);
+
+    std::vector<ncnn::Mat> weights(0);
+
+    ncnn::Mat b;
+    int ret = test_layer_naive(ncnn::LayerType::ShuffleChannel, pd, weights, a, b, 0);
+    if (ret != 0)
+    {
+        fprintf(stderr, "test_shufflechannel_pack4_non_optimal failed reference\n");
+        return ret;
+    }
+
+    ncnn::Option opt;
+    opt.num_threads = 1;
+    opt.use_packing_layout = true;
+
+    ncnn::Mat a4;
+    ncnn::convert_packing(a, a4, 4, opt);
+
+    ncnn::Layer* op = ncnn::create_layer_cpu(ncnn::LayerType::ShuffleChannel);
+    op->load_param(pd);
+
+    ncnn::ModelBinFromMatArray mb(weights.data());
+    op->load_model(mb);
+
+    op->create_pipeline(opt);
+
+    ncnn::Mat c4;
+    ret = op->forward(a4, c4, opt);
+
+    op->destroy_pipeline(opt);
+
+    delete op;
+
+    if (ret != 0 || c4.empty())
+    {
+        fprintf(stderr, "test_shufflechannel_pack4_non_optimal failed ret=%d top_blob.empty=%d\n", ret, c4.empty());
+        return -1;
+    }
+
+    ncnn::Mat c;
+    ncnn::convert_packing(c4, c, 1, opt);
+
+    if (CompareMat(b, c, 0.001f) != 0)
+    {
+        fprintf(stderr, "test_shufflechannel_pack4_non_optimal failed output mismatch\n");
+        return -1;
+    }
+
+    return 0;
+}
+#endif // NCNN_RVV || NCNN_XTHEADVECTOR
 
 static int test_shufflechannel_0()
 {
@@ -100,6 +166,9 @@ int main()
     SRAND(7767517);
 
     return 0
+#if NCNN_RVV || NCNN_XTHEADVECTOR
+           || test_shufflechannel_pack4_non_optimal()
+#endif
            || test_shufflechannel_2()
            || test_shufflechannel_0()
            || test_shufflechannel_1();
